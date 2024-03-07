103cac6ef0a5b4f2c7aa25cd5ce00b4d4b5ad41d
Sanket Jaiswal <124651819+CODEwithSANKETj@users.noreply.github.com>
2024-03-07 17:38:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT||5000
 app.use(express.json())
 app.use(cookie_parser())
 app.use(cors({
+   //  origin:'http://localhost:5173',
     credentials: true // Allow credentials (cookies) to be included in cross-origin requests
   }));
 app.get('/',(req,res)=>{

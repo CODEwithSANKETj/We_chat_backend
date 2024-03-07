90bc7ab2431b7bdd9ffd926d5430cee216ced5d4
Sanket Jaiswal <124651819+CODEwithSANKETj@users.noreply.github.com>
2024-03-07 18:22:28 +0530

    Update socketio.js

--- backend/socket/socketio.js
+++ backend/socket/socketio.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173'],
+        origin: ['https://beautiful-brigadeiros-2948c7.netlify.app'],
         methods: ['GET', 'POST'],
         credentials: true
     }
